bfee8c8778b01a2bb8a3b4c6266cd0d800417045
Samuel Silva <samuel7silva@outlook.com>
2024-07-11 20:40:50 -0300

    feat: add box shadow and bg image and logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      boxShadow: {
+        shape:
+          '0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)',
+      },
+      backgroundImage: {
+        pattern: 'url(/assets/bg.png)',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
