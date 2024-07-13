5fe69a343ca3f6dc2196a354b050bac031f935f4
Samuel Silva <samuel7silva@outlook.com>
2024-07-13 12:49:48 -0300

    fix: component style

--- src/components/confirm-trip-modal.tsx
+++ src/components/confirm-trip-modal.tsx
@@ -10,7 +10,7 @@ interface ConfirmTripModalProps {
 export function ConfirmTripModal(props: ConfirmTripModalProps) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/60 ">
-      <div className="w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
+      <div className="w-[640px] space-y-2.5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <h2 className="text-lg font-semibold">
@@ -32,7 +32,7 @@ export function ConfirmTripModal(props: ConfirmTripModalProps) {
         </div>
         <div className="flex flex-wrap gap-2"></div>
         <form onSubmit={props.createTrip} className="space-y-3">
-          <div className="flex h-14 items-center gap-2 rounded-md border-zinc-800 bg-zinc-950 px-1.5">
+          <div className="flex h-14 items-center gap-2 rounded-lg border-zinc-800 bg-zinc-950 px-4">
             <User className="h-5 w-5 text-zinc-400" />
             <input
               className="placeholder-bg-zinc-400 flex-1 bg-transparent text-lg outline-none"
@@ -41,7 +41,7 @@ export function ConfirmTripModal(props: ConfirmTripModalProps) {
             />
           </div>
 
-          <div className="flex h-14 items-center gap-2 rounded-md border-zinc-800 bg-zinc-950 px-1.5">
+          <div className="flex h-14 items-center gap-2 rounded-lg border-zinc-800 bg-zinc-950 px-4">
             <Mail className="h-5 w-5 text-zinc-400" />
             <input
               className="placeholder-bg-zinc-400 flex-1 bg-transparent text-lg outline-none"
